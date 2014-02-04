7b42201097fc7da386221418ec22167453cb3afa
David R. Andersen <k0rx@RXcomm.net>
2014-02-04 10:34:04 -0600

    bug in long message code fixed

--- weechat-axolotl.py
+++ weechat-axolotl.py
@@ -93,13 +93,13 @@ def encrypt(data, msgtype, servername, args):
     if len(encrypted) > 400:
       splitmsg=string.split(message," ")
       cutpoint=len(splitmsg)/2
-      p = subprocess.Popen([weechat_dir + '/python/axolotl.worker.py', '-e', weechat_dir, weechat.config_get_plugin('axo_id'), username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+      p = subprocess.Popen([weechat_dir + '/python/axolotl.worker.py', '-e', nick, username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
       p.stdin.write(string.join(splitmsg[:cutpoint]," ") + "\n")
       p.stdin.close()
       encrypted = p.stdout.read()
       encrypted = encrypted.replace("\n","")
       p.stdout.close()
-      p = subprocess.Popen([weechat_dir + 'python/axolotl.worker.py', '-e', weechat_dir, weechat.config_get_plugin('axo_id'), username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
+      p = subprocess.Popen([weechat_dir + 'python/axolotl.worker.py', '-e', weechat_dir, nick, username], bufsize=4096, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
       p.stdin.write( string.join(splitmsg[cutpoint:]," ") )
       p.stdin.close()
       encrypted2 = p.stdout.read()
